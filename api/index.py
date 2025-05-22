aa701847dfd42a4756ef3289b2222611940620e8
codingartist58 <sarumonica58@gmail.com>
2025-05-22 13:14:25 +0530

    dispaly marks for request

--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
-
+import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         # read the data file
         file = open('q-vercel-python.json', 'r')
         data = file.read()
+        data = json.loads(data)
         file.close()
 
 
@@ -20,11 +21,22 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        response_body = "Request Parameters:\n"
+        response_body = ""
+        request_array = []
+        marks_dict = {"marks" : []}
 
         for key, value in query_params.items():
-            response_body += f"{key}: {value}\n"
+            #response_body += f"{key}: {value}\n"
+            if key == 'name':
+                request_array = value
 
-        response_body += f"\nData from file:\n {data}"
+        for req in request_array:
+            for dict in data:
+                #print(type(dict))
+                if dict["name"] == req:
+                    marks_dict["marks"].append(dict["marks"])
+                    break
+
+        response_body += f"{str(marks_dict)}"
         self.wfile.write(response_body.encode('utf-8'))
         return
